00a2b92590e3a4d29171fd73b3ec23e83a2a51f0
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-02-08 19:32:14 +0530

    8th Feb - 1st commit

---
+++ src/Excercise/Arithmetic_Operators.java
@@ -0,0 +1,25 @@
+package Excercise;
+public class Arithmetic_Operators {
+ 
+ public static void main(String[] args) {
+ int a, b = 10, c = 5;
+ a = b + c;
+ boolean compare,test=false;
+ compare = (a==b);
+ System.out.println("Logical comparion of a with b "+ (compare&&test));
+ System.out.println("Value of 'a' after '+' Arithmetic operation is " + a);
+ a = b - c;
+ System.out.println("Value of 'a' after '-' Arithmetic operation is " + a);
+ a = b * c;
+ System.out.println("Value of 'a' after '*' Arithmetic operation is " + a);
+ a = b / c;
+ System.out.println("Value of 'a' after '/' Arithmetic operation is " + a);
+ a = b % c;
+ System.out.println("Value of 'a' after '%' Arithmetic operation is " + a);
+ b++;
+ System.out.println("Value of 'b' after '++' Arithmetic operation is " + b);
+ c--;
+ System.out.println("Value of 'c' after '--' Arithmetic operation is " + c);
+ }
+ 
+}
