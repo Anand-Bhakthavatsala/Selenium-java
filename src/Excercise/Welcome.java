f1c05eec3340782608b773c6e662ab1837e70016
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-02-08 19:51:18 +0530

    changes

--- src/Excercise/Welcome.java
+++ src/Excercise/Welcome.java
@@ -2,10 +2,6 @@ package Excercise;
 
 public class Welcome {
 
-	public Welcome() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Java learning");
