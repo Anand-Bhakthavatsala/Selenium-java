37bb33ebbfcee8a167095144e85a705e2e6428b1
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-02-08 20:15:05 +0530

    welcome2

---
+++ src/Excercise/Welcome2.java
@@ -0,0 +1,10 @@
+package Excercise;
+
+public class Welcome2 {
+
+	public static void main(String[] args) {
+		// printing the welcome message
+		System.out.println("Welcome to Java learning");
+	}
+
+}
