c4733a7cd81c04526c6c8798c3be9e7b5570eb03
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-02-09 09:01:30 +0530

    1st commit

---
+++ src/Excercise/Hello.java
@@ -0,0 +1,10 @@
+package Excercise;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// printing the welcome message
+		System.out.println("Hello, Welcome to Java learning");
+	}
+
+}
