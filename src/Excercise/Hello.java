51096efc2bb7ad67066921b5621901d85e716ea9
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-06-23 17:35:32 +0530

    Hello commit

--- src/Excercise/Hello.java
+++ src/Excercise/Hello.java
@@ -3,8 +3,9 @@ package Excercise;
 public class Hello {
 
 	public static void main(String[] args) {
-		// printing the welcome message
+		// printing the welcome message - 23rd June
 		System.out.println("Hello, Welcome to Java learning");
+		System.out.println("Today is a new learning");
 	}
 
 }
